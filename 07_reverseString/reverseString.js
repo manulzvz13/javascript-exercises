78edede69e982b3ab8ff974b310756f99bac2c6e
manulzvz13 <manulzvz@gmail.com>
2025-05-22 21:32:05 -0600

    feat: Solve 07_reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const newWord = str
+                    .split('')
+                    .reverse()
+                    .join('');
+    return newWord;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
