97563780081f18ddc2cb9c62ec8993d54c0667d1
manulzvz13 <manulzvz@gmail.com>
2025-05-23 18:32:56 -0600

    feat: Solve 09_sumAll Exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (intOne, intTwo) {
+  if (!Number.isInteger(intOne) || !Number.isInteger(intTwo)) {
+    return "ERROR";
+  }
+  if (intOne > 0 && intTwo > 0) {
+    let sum = 0;
+    if (intOne < intTwo) {
+      for (let i = intOne; i <= intTwo; i++) {
+        sum += i;
+      }
+      return sum;
+    } else {
+      for (let i = intTwo; i <= intOne; i++) {
+        sum += i;
+      }
+      return sum;
+    }
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
