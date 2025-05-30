33e7ab988d11b6d8c45fb8acc75e63975cbc6215
manulzvz13 <manulzvz@gmail.com>
2025-05-30 08:27:19 -0600

    feat: Solve 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/\s+/g, '');
+    let str1 = str.replace(/[,!.;?]/g, ''); 
+    let str2 = Array.from(str1).reverse().join('');
+    if (str1 === str2){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
