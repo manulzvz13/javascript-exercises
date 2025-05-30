8f4c812f5db8a387715731f0813ee7229df074a5
manulzvz13 <manulzvz@gmail.com>
2025-05-30 08:35:43 -0600

    refactor: combine regex into single replace for palindrome check

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,7 +1,7 @@
 const palindromes = function (str) {
-    str = str.toLowerCase();
-    str = str.replace(/\s+/g, '');
-    let str1 = str.replace(/[,!.;?]/g, ''); 
+    str1 = str
+            .toLowerCase()
+            .replace(/[\s,!.;?]/g, ''); 
     let str2 = Array.from(str1).reverse().join('');
     if (str1 === str2){
         return true;
