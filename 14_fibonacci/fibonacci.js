a2d9801e4908b51edf2b5a2f0c1b67701ae5bc84
manulzvz13 <manulzvz@gmail.com>
2025-06-02 12:55:23 -0600

    feat: Solve 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let count;
+  if (typeof num !== "number") {
+    count = parseInt(num);
+  } else {
+    count = num;
+  }
+  if (count < 0) return "OOPS";
+  if (count == 0) return 0;
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= count; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 };
 
 // Do not edit below this line
